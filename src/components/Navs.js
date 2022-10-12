e7116bb7f65b1d1e961bd474055556e5f2f17002
Woojin <bwj59@naver.com>
2022-10-13 00:14:23 +0900

    TN7-127 [FIX] 로그인 axios 코드 수정(고침)

--- src/components/Navs.js
+++ src/components/Navs.js
@@ -5,15 +5,17 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { useDispatch  } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 function Navs() {
 
     const dispatch = useDispatch()
 
     const onLogout = () => {
-    	// sessionStorage 에 email 로 저장되어있는 아이템을 삭제한다.
+        // sessionStorage 에 email 로 저장되어있는 아이템을 삭제한다.
         localStorage.removeItem('email')
+        localStorage.removeItem('token')
+        localStorage.removeItem('name')
         // App 으로 이동(새로고침)
         document.location.href = '/'
     }
@@ -38,7 +40,7 @@ function Navs() {
                     </Nav>
                     <Nav>
                         {/*localStorage의 값을 확인해서 존재하면 실행시킴  */}
-                        {localStorage.getItem('email') ? <> 
+                        {localStorage.getItem('email') ? <>
                             <NavDropdown title={`반갑습니다! ${localStorage.getItem('name')}님`} id="collasible-nav-dropdown">
                                 <NavDropdown.Item href="/null1">Action1</NavDropdown.Item>
                                 <NavDropdown.Item href="/null2">Action2</NavDropdown.Item>
@@ -47,13 +49,13 @@ function Navs() {
                                 <NavDropdown.Item href="/" onClick={onLogout}>로그아웃</NavDropdown.Item>
                             </NavDropdown>
                         </> :
-                        <>{/*localStorage의 값을 확인해서 로그인이 되어있으면 회원가입이랑 로그인은 안보이게함  */}
-                            <Nav.Link href="/Signin">Signin</Nav.Link>
-                            <Nav.Link eventKey={2} href="/login">Login</Nav.Link>
-                        </>
-                        } 
-                        
-                        
+                            <>{/*localStorage의 값을 확인해서 로그인이 되어있으면 회원가입이랑 로그인은 안보이게함  */}
+                                <Nav.Link href="/Signin">Signin</Nav.Link>
+                                <Nav.Link eventKey={2} href="/login">Login</Nav.Link>
+                            </>
+                        }
+
+
                     </Nav>
                 </Navbar.Collapse>
             </Container>
