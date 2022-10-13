645c2d1ae29d47e9e9c3a6ebd0f88e0b1ca016e9
bluesushi264 <bluesushi264@gmail.com>
2022-10-14 00:28:34 +0900

    [FIX] 카카오 로그인 API 수정중

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Signin from './components/Signin';
 import Dashboard from './components/Dashboard';
 import Mksurvey from './components/Mksurvey';
 
+import {KAKAO_AUTH_URL} from './components/OAuth';
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom' // 리액트 라우터 기능 사용
 
 
@@ -19,6 +21,10 @@ function App() {
         <Route path="/" element={<><Main /></>}></Route>
         <Route path="/signin" element={<><Signin /></>}></Route>
         <Route path="/login" element={<><Login /></>}></Route>
+        <Route path='/kakaologin' component={() => {
+     window.location.href = {KAKAO_AUTH_URL};
+     return null;
+}}/>
         {localStorage.getItem('email') //로그인된 여부에 따라 경로를 다르게 설정함
           ? <Route path='/dashboard' element={<><Dashboard /></>}></Route>
           : <Route path='/dashboard' element={<><Login /></>}></Route>
