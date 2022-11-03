5abe0e3470903ba33f519333e15d53fa44f5ca73
WOOJIN BAEK <45286570+bwj0509@users.noreply.github.com>
2022-11-03 20:58:31 +0900

    TN7-204 [DESIGN] 설문 응답시 보여지는 화면 디자인 수정

--- src/components/Dosurvey.js
+++ src/components/Dosurvey.js
@@ -4,6 +4,9 @@ import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import Slider from '@mui/material/Slider';
 
+import { ReactComponent as EmotionGood } from "../img/emotiongood.svg";
+import { ReactComponent as EmotionBad } from "../img/emotionbad.svg";
+
 const BackgroundDiv = styled.div`
   background: #301e4e;
   width: 100%;
@@ -14,24 +17,23 @@ const BackgroundDiv = styled.div`
 `
 
 const StartCard = styled.div`
-    background-color: #ff6e6c;
-    color:white;
-    border: 3px solid black;
-    width: 600px;
-    padding: 20px;
-    border-radius: 15px;
+    background-color: rgba(255, 255, 255, 0.8);
+    color:black;
+    width: 700px;
+    padding: 30px;
+    border-radius: 40px;
 
 `
 const SurveyCard = styled.div`
-    background-color: #ff6e6c;
-    color:white;
-    width: 600px;
+    background-color: rgba(255, 255, 255, 0.8);
+    color:black;
+    width: 700px;
     padding: 30px;
-    border-radius: 15px;
+    border-radius: 40px;
     
 `
 const QuestionTitle = styled.div`
-  font-size: 1.5rem;
+  font-size: 2rem;
 `
 
 
@@ -39,8 +41,11 @@ const LinerBtn = styled.button`
   color: ${props => props.checked ? 'white' : 'black'};
   background:${props => props.checked ? 'black' : '#e1e1e1'} ;
   border:0px;
-  width: 40px;
+  width: 120px;
+  height: 100px;
   border-radius: 5px;
+  font-size: 30px;
+  font-weight: bold;
 
   &:hover {
     background: #000000;
@@ -48,6 +53,65 @@ const LinerBtn = styled.button`
   }
 `
 
+const EmotionSlider = styled(Slider)({
+  color: '#52af77 !important', //important를 이용해서 css우선순위를 1순위로 끌어올렸다.
+  height: 8,
+  '& .MuiSlider-track': {
+    border: 'none',
+  },
+  '& .MuiSlider-thumb': {
+    height: 24,
+    width: 24,
+    backgroundImage: "url('../img/emotionbad.svg') !important",
+    border: '2px solid currentColor',
+    '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible': {
+      boxShadow: 'inherit',
+    },
+    '&:before': {
+      display: 'none',
+    },
+  },
+  '& .MuiSlider-valueLabel': {
+    lineHeight: 1.2,
+    fontSize: 12,
+    background: 'unset',
+    padding: 0,
+    width: 32,
+    height: 32,
+    borderRadius: '50% 50% 50% 0',
+    backgroundColor: '#52af77',
+    transformOrigin: 'bottom left',
+    transform: 'translate(50%, -100%) rotate(-45deg) scale(0)',
+    '&:before': { display: 'none' },
+    '&.MuiSlider-valueLabelOpen': {
+      transform: 'translate(50%, -100%) rotate(-45deg) scale(1)',
+    },
+    '& > *': {
+      transform: 'rotate(45deg)',
+    },
+  },
+});
+
+const StartSurveyBtn = styled.button`
+  margin: 20px auto;
+  display: block;
+  border: 0px;
+  width: 200px;
+  height: 50px;
+  background: black;
+  color:white;
+  font-size: 1.1rem;
+  border-radius: 20px;
+  &:hover {
+    background: rgba(255, 255, 255, 0.5);
+    color:black
+  }
+  transition:all 200ms linear;
+`
+const AnswerInput = styled.input`
+  background: rgba(255, 255, 255, 0.2);
+  height: 40px;
+`
 
 function Dosurvey() {
   const params = useParams();
@@ -57,7 +121,7 @@ function Dosurvey() {
   const [showSurveyNumber, setShowSurveyNumber] = useState(0); //현재 답변중인 문항 번호
 
   const [result, setResult] = useState('')//설문의 결과를 배열로 저장하는 state
-  const [inputs, setInputs]= useState(); // 현재 설문 문항에 대한 데이터를 가지고 있는 state!
+  const [inputs, setInputs] = useState(); // 현재 설문 문항에 대한 데이터를 가지고 있는 state!
 
   useEffect(() => {
     setLoading(true)
@@ -79,9 +143,9 @@ function Dosurvey() {
   }, [])
 
 
-  useEffect(()=>{
-    console.log('inputs이 변화했습니다',inputs)
-  },[inputs])
+  useEffect(() => {
+    console.log('inputs이 변화했습니다', inputs)
+  }, [inputs])
 
 
   if (loading) return (
@@ -125,11 +189,11 @@ function Dosurvey() {
     if (surveyDetail.questions[showSurveyNumber - 1].answerRequired) { // 필수문항이면
       if (showSurveyNumber < surveyDetail.questions.length && result[showSurveyNumber - 1] !== undefined) { //마지막 문항이 아니고 && 설문에 응답했으면
         setShowSurveyNumber(showSurveyNumber + 1) // 다음 문항으로 넘어가줘라
-        if(result[showSurveyNumber]===undefined){ // 다음 문항에 내용이 아직 없다면
+        if (result[showSurveyNumber] === undefined) { // 다음 문항에 내용이 아직 없다면
           setInputs('') // input창에 아무것도 안보여주고
         }
-        else{
-        setInputs(result[showSurveyNumber]) // inputs 값에 이미 답변한 내용을 보여줘라!
+        else {
+          setInputs(result[showSurveyNumber]) // inputs 값에 이미 답변한 내용을 보여줘라!
         }
       }
       else {
@@ -141,20 +205,20 @@ function Dosurvey() {
         if (result[showSurveyNumber - 1] === undefined) { // 문항 답변에 아무것도 없을 경우에
           result[showSurveyNumber - 1] = null // 널값을 집어넣고
           setShowSurveyNumber(showSurveyNumber + 1)//다음 문항으로 넘어간다
-          if(result[showSurveyNumber]===undefined){ // 다음 문항에 내용이 아직 없다면
+          if (result[showSurveyNumber] === undefined) { // 다음 문항에 내용이 아직 없다면
             setInputs('') // input창에 아무것도 안보여주고
           }
-          else{
-          setInputs(result[showSurveyNumber]) // inputs 값에 이미 답변한 내용을 보여줘라!
+          else {
+            setInputs(result[showSurveyNumber]) // inputs 값에 이미 답변한 내용을 보여줘라!
           }
         }
-        else{
-          setShowSurveyNumber(showSurveyNumber+1) //답변이 있을경우 그냥 다음 문제로 넘어간다.
-          if(result[showSurveyNumber]===undefined){ // 다음 문항에 내용이 아직 없다면
+        else {
+          setShowSurveyNumber(showSurveyNumber + 1) //답변이 있을경우 그냥 다음 문제로 넘어간다.
+          if (result[showSurveyNumber] === undefined) { // 다음 문항에 내용이 아직 없다면
             setInputs('') // input창에 아무것도 안보여주고
           }
-          else{
-          setInputs(result[showSurveyNumber]) // inputs 값에 이미 답변한 내용을 보여줘라!
+          else {
+            setInputs(result[showSurveyNumber]) // inputs 값에 이미 답변한 내용을 보여줘라!
           }
         }
       }
@@ -169,7 +233,7 @@ function Dosurvey() {
     }
   }
 
-  const onChangeType1 = (e)=>{ // 장문 문항에 대한 핸들링
+  const onChangeType1 = (e) => { // 장문 문항에 대한 핸들링
     let temparr = result
     temparr[showSurveyNumber - 1] = e.target.value
     setResult(temparr)
@@ -197,68 +261,70 @@ function Dosurvey() {
   }
 
 
-  
+
   return (
     <BackgroundDiv>
-      { showSurveyNumber === 0 //설문 시작화면 보여주기
+      {showSurveyNumber === 0 //설문 시작화면 보여주기
         &&
         <StartCard>
-        <div>디자인작업 진행 0%, 로직 진행도 40%, 위에 NAV안나오게 해야함</div>
-        <div>설문번호 : {params.surveyId}</div>
-        <div>설문이름 : {surveyDetail.survey.name}</div>
-        <div>설문문항수 : {surveyDetail.questions.length + surveyDetail.choiceQuestions.length}</div>
-        <hr />
-        <div>설문을 시작하시겠습니까?</div>
-        <button onClick={startSurvey}>설문시작</button>
-        <div>{showSurveyNumber}</div>
-        {console.log(surveyDetail)}
-      </StartCard>
+
+          <QuestionTitle><span style={{ fontWeight: 'bold' }}>{surveyDetail.survey.name}</span>에 관한 설문입니다.</QuestionTitle>
+          <QuestionTitle>설문 문항은 총 <span style={{ fontWeight: 'bold' }}>{surveyDetail.questions.length + surveyDetail.choiceQuestions.length}문항</span> 입니다. </QuestionTitle>
+          <StartSurveyBtn onClick={startSurvey}>설문시작</StartSurveyBtn>
+          <hr />
+          <div>개발자 참고 공간 ↓</div>
+          <div>설문번호 : {params.surveyId}</div>
+
+          <div>디자인작업 진행 10%, 로직 진행도 40%, 위에 NAV안나오게 해야함</div>
+          {console.log(surveyDetail)}
+        </StartCard>
       }
-      
+
       <div>
         {showSurveyNumber >= 1 //1번 문제부터 보여주기
           &&
           <SurveyCard>
             <QuestionTitle>{`${showSurveyNumber}. ${surveyDetail.questions[showSurveyNumber - 1].name}`}</QuestionTitle>
-
+            <br />
             {surveyDetail.questions[showSurveyNumber - 1].questionTypeId === 1 && //1번 타입의 문항(장문)을 경우 아래의 식을 수행
-              <input style={{ width: '100%' }} name={showSurveyNumber} onChange={onChangeType1} value={inputs}></input>
+              <AnswerInput style={{ width: '100%', type: 'textarea' }} name={showSurveyNumber} onChange={onChangeType1} value={inputs}></AnswerInput>
             }
             {surveyDetail.questions[showSurveyNumber - 1].questionTypeId === 2 && //2번 타입의 문항(단문)을 경우 아래의 식을 수행
-              <input style={{ width: '50%' }} name={showSurveyNumber} onChange={onChangeType2} value={inputs}></input>
+              <AnswerInput style={{ width: '70%' }} name={showSurveyNumber} onChange={onChangeType2} value={inputs}></AnswerInput>
             }
             {surveyDetail.questions[showSurveyNumber - 1].questionTypeId === 5 && //5번 타입의 문항(감정바)을 경우 아래의 식을 수행
               <div style={{ width: '500px' }}>
-                <Slider onChange={onChangeType5} valueLabelDisplay="auto" value={inputs} />
+                <EmotionSlider onChange={onChangeType5} valueLabelDisplay="auto" value={inputs} />
               </div>
             }
             {surveyDetail.questions[showSurveyNumber - 1].questionTypeId === 6 && //6번 타입의 문항(선형배율)을 경우 아래의 식을 수행
-              <>
-                <LinerBtn checked={inputs==='1'?true:false} name='1' onClick={onChangeType6} >1 </LinerBtn>
-                <LinerBtn checked={inputs==='2'?true:false} name='2' onClick={onChangeType6}>2 </LinerBtn>
-                <LinerBtn checked={inputs==='3'?true:false} name='3' onClick={onChangeType6}>3 </LinerBtn>
-                <LinerBtn checked={inputs==='4'?true:false} name='4' onClick={onChangeType6}>4 </LinerBtn>
-                <LinerBtn checked={inputs==='5'?true:false} name='5' onClick={onChangeType6}>5 </LinerBtn>
-              </>
-              
-            }
-            {console.log(`result : ${result}`)}
+              <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <LinerBtn checked={inputs === '1' ? true : false} name='1' onClick={onChangeType6} >1<br /><span style={{ fontSize: '11px' }}>매우 그렇지 않다</span></LinerBtn>
+                <LinerBtn checked={inputs === '2' ? true : false} name='2' onClick={onChangeType6}>2<br /><span style={{ fontSize: '11px' }}>그렇지 않다</span> </LinerBtn>
+                <LinerBtn checked={inputs === '3' ? true : false} name='3' onClick={onChangeType6}>3<br /><span style={{ fontSize: '11px' }}>보통이다</span> </LinerBtn>
+                <LinerBtn checked={inputs === '4' ? true : false} name='4' onClick={onChangeType6}>4<br /><span style={{ fontSize: '11px' }}>약간 그렇다</span></LinerBtn>
+                <LinerBtn checked={inputs === '5' ? true : false} name='5' onClick={onChangeType6}>5<br /><span style={{ fontSize: '11px' }}>매우 그렇다</span> </LinerBtn>
+              </div>
 
+            }
+            <br />
+            {console.log(`result : ${result}`)}
+            <br />
             {showSurveyNumber === surveyDetail.questions.length // 설문의 마지막 문항일때 조건
               ?
-              <div style={{display:'flex', justifyContent:'flex-end'}}>
+              <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
                 <button onClick={prevQuestion}>이전문항</button>
                 <button onClick={onSubmit}>제출하기</button>
               </div>
 
               :
-              <div style={{display:'flex', justifyContent:'flex-end'}}>
+              <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
                 <button onClick={prevQuestion}>이전문항</button>
                 <button onClick={nextQuestion}>다음문항</button>
               </div>
             }
 
-            <hr/>
+            <hr />
             <div>개발자 참고 공간 ↓</div>
             <div>{`question타입 : ${surveyDetail.questions[showSurveyNumber - 1].questionTypeId}`}</div>
             <div>{`필수답변여부 : ${surveyDetail.questions[showSurveyNumber - 1].answerRequired}`}</div>
