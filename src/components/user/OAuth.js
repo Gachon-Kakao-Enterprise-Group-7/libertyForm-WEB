992f02d20f8c3d48406211af1f639f303f659004
WOOJIN BAEK <45286570+bwj0509@users.noreply.github.com>
2022-12-08 17:35:25 +0900

    [FIX] 환경변수 수정

--- src/components/user/OAuth.js
+++ src/components/user/OAuth.js
@@ -1,3 +1,3 @@
 export const REST_API_KEY = "9769938ebf0f18edb3d2c8b7ab2abb60";
-export const REDIRECT_URI = "http://localhost:3000/kakaologin";
+export const REDIRECT_URI = `${process.env.REACT_APP_KAKAO_REDIRECT_URI}kakaologin`;
 // 인증에 중요한 정보는 따로 js파일을 만들어서 관리
