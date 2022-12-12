c067e391bfac5a147a23ca63691144b9d06db5dd
“gayoung” <tp06100@naver.com>
2022-12-12 18:38:17 +0900

    sendsurvey폴더 수정

--- src/components/sendsurvey/Groupcontrol.js
+++ src/components/sendsurvey/Groupcontrol.js
@@ -96,83 +96,47 @@ function Groupcontrol(props) {
 
   return (
     <div>
-      <Title>그룹에서 선택</Title>
-      <GroupControll>
-        <Autocomplete // 그룹 선택하는 콤보 박스
-          value={groupValue}
-          onChange={(event, newGroupValue) => {
-            setGroupValue(newGroupValue);
-          }}
-          id="controllable-states-demo"
-          options={options}
-          sx={{ width: "50%", m: 3 }}
-          renderInput={(params) => (
-            <TextField {...params} label="그룹을 선택하세요" />
-          )}
-        />
-        {groupValue !== null && (
-          <AddBtn onClick={addAlluser}>
-            <UserAddSvgW />
-            <p>전체 추가</p>
-          </AddBtn>
-        )}
-      </GroupControll>
-      {groupValue != null && (
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 350 }} aria-label="simple table">
-            <TableHead>
-              <TableRow style={{ color: "red" }}>
-                <TableCell
-                  align="center"
-                  size="small"
-                  style={{ fontWeight: "bold" }}
-                >
-                  추가
-                </TableCell>
-                <TableCell align="center" style={{ fontWeight: "bold" }}>
-                  이름
-                </TableCell>
-                <TableCell align="center" style={{ fontWeight: "bold" }}>
-                  이메일
-                </TableCell>
-                <TableCell align="center" style={{ fontWeight: "bold" }}>
-                  회원여부
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            {state
-              .filter((contact) => contact.relationship === groupValue)
-              .map((contact, index) => (
-                <TableBody key={index}>
-                  <TableCell align="center" padding="none">
-                    <StyledAdd
-                      width="15px"
-                      height="30px"
-                      onClick={() => {
-                        props.setUsers((prev) => [
-                          ...new Set([...prev, contact.email]),
-                        ]);
-                      }}
-                      cursor="pointer"
-                    ></StyledAdd>
-                  </TableCell>
-                  <TableCell align="center" padding="none">
-                    {contact.name}
-                  </TableCell>
-                  <TableCell align="center" padding="none">
-                    {contact.email}
-                  </TableCell>
-                  <TableCell align="center" padding="none">
-                    {contact.member && <Check width="20px" />}
-                  </TableCell>
-                  {/*위에 addUser 랑 같은 이벤트 : set을 이용해서 이미 사용자가 있으면 중복 제거된다 */}
-                </TableBody>
-              ))}
-          </Table>
-        </TableContainer>
-      )}
-    </div>
-  );
+    <Title>그룹에서 선택</Title>
+    <GroupControll>
+      <Autocomplete // 그룹 선택하는 콤보 박스
+        value={groupValue}
+        onChange={(event, newGroupValue) => {
+          setGroupValue(newGroupValue);
+        }}
+        id="controllable-states-demo"
+        options={options}
+        sx={{ width: '50%', m: 3 }}
+        renderInput={(params) => <TextField {...params} label="그룹을 선택하세요" />}
+      />
+      {groupValue !== null &&
+        <AddBtn onClick={addAlluser}><UserAddSvgW /><p>전체 추가</p></AddBtn>}
+    </GroupControll>
+    {groupValue != null &&
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 350, }} aria-label="simple table">
+          <TableHead>
+            <TableRow style={{ color: 'red' }}>
+              <TableCell align='center' size='small' style={{ fontWeight: 'bold' }}>추가</TableCell>
+              <TableCell align='center' style={{ fontWeight: 'bold' }}>이름</TableCell>
+              <TableCell align='center' style={{ fontWeight: 'bold' }}>이메일</TableCell>
+              <TableCell align='center' style={{ fontWeight: 'bold' }}>회원여부</TableCell>
+
+            </TableRow>
+          </TableHead>
+          {state.filter((contact) => (contact.relationship === groupValue)).map((contact, index) => (
+            <TableBody key={index}>
+              <TableCell align='center' padding='none' ><StyledAdd width='15px' height='30px' onClick={() => { props.setUsers((prev) => ([...new Set([...prev, contact.email])])) }} cursor='pointer'></StyledAdd></TableCell>
+              <TableCell align='center' padding='none'>{contact.name}</TableCell>
+              <TableCell align='center' padding='none'>{contact.email}</TableCell>
+              <TableCell align='center' padding='none'>{contact.member && <Check width='20px' />}</TableCell>
+              {/*위에 addUser 랑 같은 이벤트 : set을 이용해서 이미 사용자가 있으면 중복 제거된다 */}
+            </TableBody>
+          ))}
+        </Table>
+      </TableContainer>
+    }
+  </div>
+);
 }
 
 export default Groupcontrol;
